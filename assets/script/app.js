b1ffb0b7161f697c272626d3bd451f2f07565011
Yuliia Savdeieva <yuliia.savdeieva@Yuliias-MacBook-Pro.local>
2020-04-30 19:07:57 +0300

    third animation

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,5 +1,11 @@
+
+
+
+
 window.onload = (e) => {
     e.preventDefault();
+
+    //FIRST ANIMATION (CIRCLES)
     const leftTopCircleMove = document.querySelector(".circle-plain-left");
     const rigthTopCircleMove = document.querySelector(".circle-plain-right");
     const leftBottomCircleMove = document.querySelector(".circle-plain-left-bottom");
@@ -48,6 +54,86 @@ window.onload = (e) => {
         leftTopCircle.style.fill = "#efefef";
         rightTopHalfCircle.style.fill = "#efefef";
     }, 2200)
-
     
 }
+
+// THIRD ANIMATION 
+window.addEventListener('scroll', () => {
+    let scrollValue = window.pageYOffset;
+    const secondSection =  document.querySelector("section.about").offsetHeight;
+    const leftTopBigObj = document.querySelector(".block-left-bottom");
+    const leftBottomBigObj = document.querySelector("rect#Block-Utekai");
+    const leftTopSmall = document.querySelector("rect#Block-Left-Top");
+    const centerTopLarge = document.querySelector("rect#Block-Center-Top-Left-L");
+    const centerSmall = document.querySelector("rect#Square-CENTER");
+    const centerRightL = document.querySelector("rect#Block-Center-Right-L");
+    const blackBlocks = document.querySelectorAll(".black");
+    const rigthBlock = document.querySelector("#Rigth-Block");
+    const allLines = document.querySelectorAll(".st0");
+    const thirdAnimationSrart = (secondSection / 8) + secondSection;
+    
+    
+    if(scrollValue >= thirdAnimationSrart)
+    {
+
+        let value = (scrollValue - thirdAnimationSrart) * 4;
+        leftTopBigObj.style.transform = `translateY(-${value}px)`;
+        leftBottomBigObj.style.transform = `translateY(${1400 - value}px)`
+        if(value >= 50)
+        {
+            leftTopSmall.style.transform = `translateY(${1400 - (value - 100)}px)`
+            centerTopLarge.style.transform = `translateY(${1400 - (value - 200)}px)`;
+        }
+        if(value >= 100)
+        {
+            centerSmall.style.transform = `translateY(${790 - (value - 800)}px)`
+            centerRightL.style.transform = `translateY(${790 - (value - 1000)}px)`
+            
+        }
+        if (value >= 150) {
+            rigthBlock.style.transform = `translateY(${1400 - (value - 600)}px)`
+        }
+        if(value >= 1400) {
+            leftTopBigObj.style.transform = `translateY(-1400px)`;
+            leftBottomBigObj.style.transform = `translateY(0px)`;
+        }
+        if(value >= 1600)
+        {
+            centerSmall.style.transform = `translateY(0px)`;
+            blackBlocks.forEach(el => {
+                el.style.stroke = `black`
+            })
+            allLines.forEach(line => {
+                line.style.stroke = `transparent`;
+            });
+        } else {
+            blackBlocks.forEach(el => {
+                el.style.stroke = `#efefef`
+            })
+            allLines.forEach(line => {
+                line.style.stroke = `#efefef`;
+            });
+        }
+        if(value >= 1800)
+        {
+            centerRightL.style.transform = `translateY(0px)`;
+        }
+        if (value >= 2000)
+        {
+            rigthBlock.style.transform = `translateY(0px)`;
+        }
+        if(value >= 1200)
+        {
+            centerTopLarge.style.transform = `translateY(360px)`;
+        }
+        if(value - 100 >= 1400)
+        {
+            leftTopSmall.style.transform = `translateY(0px)`;
+        }
+
+    }
+    
+})
+
+// THIRD ANIMATION 
+// FOURTH ANIMATION 
