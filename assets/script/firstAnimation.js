01d018cd2c3bb55e7d465a35d1f4d273b9ff5dee
Yuliia Savdeieva <yuliia.savdeieva@Yuliias-MacBook-Pro.local>
2020-05-09 18:37:31 +0300

    fixing

--- assets/script/firstAnimation.js
+++ assets/script/firstAnimation.js
@@ -45,6 +45,7 @@ window.onload = (e) => {
         {
             leftTopCircleMove.style.animation = "left-right-top-1920 2s cubic-bezier(0.76, 0, 0.24, 1)";
             rigthTopCircleMove.style.animation = "right-left-top-1920 2s ease";
+            console.log('window.innerWidth >= 1920');
         } else if (window.innerWidth >= 1440 && window.innerWidth < 1920)
         {
             leftTopCircleMove.style.animation = "left-right-top-1440 2s cubic-bezier(0.76, 0, 0.24, 1)";
